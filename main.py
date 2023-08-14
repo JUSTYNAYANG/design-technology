aebf77e24e86b6f97f6e3951dfdf0bbd668fe3ac
JUSTYNAYANG <103252328+JUSTYNAYANG@users.noreply.github.com>
2023-08-14 12:53:51 +0800

    Variable DataType

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-print("hello world")
+name = "Mr.Pilon"
+age = 38
+print(f"Hi my name is {name}. I am {age} years old.")
+print("Hi my name is {}. I am {} years old.".format(name, age))
 
-age = 15
+#int, float, bool
+intro = "Once upon a time there was a living ice cream. Of course, the ice cream was not always alive."
+object = True
+alive = ", like every other icecream it started out as an object.  Luckily for this icecream, it soon gain \nconsciousness."
+time = "On April"
+day = 1
+year = 2055
+awaken = ", the living icecream woke up to find that"
+ate = 0.1
+gone = ("of its body \n"
+        "was gone. Oh what a horror! Fortunately the ice cream could not feel pain. Thus, the ice cream lived \n"
+        "happily every after, exploring the new world it was now in.")
 
-print(f"I am {age} years old.")
-print("my age is " + str(age))
+theend = "\n╱╭╮╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮"\
+         "\n╭╯╰┫┃╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃"\
+         "\n╰╮╭┫╰━┳━━╮╭━━┳━╮╭━╯┃"\
+         "\n╱┃┃┃╭╮┃┃━┫┃┃━┫╭╮┫╭╮┃"\
+         "\n╱┃╰┫┃┃┃┃━┫┃┃━┫┃┃┃╰╯┃"\
+         "\n╱╰━┻╯╰┻━━╯╰━━┻╯╰┻━━╯"
 
-age += 1
+print(f"{intro} {object}\n{alive} On April {day} of the year {year}{awaken} {ate} {gone} {theend}")
 
-print(f"I will be {age} years old after september 30th.")
+print("\nOnce upon a time there was a living ice cream. Of course, the ice cream was not always alive."
+      "{} \n, like every other icecream it started out as an object.  Luckily for this icecream, it soon gain "
+      "\nconsciousness. On April {} of the year {}, the living icecream woke up to find that {} of its body"
+      "\nwas gone. Oh what a horror! Fortunately the ice cream could not feel pain. Thus, the ice cream lived"
+      "\nhappily every after, exploring the new world it was now in. {}".format(object, day, year, ate, theend))
+
+
+
