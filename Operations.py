c2bca909c83e6e7e2188be768c43f2d55d2b898f
JUSTYNAYANG <103252328+JUSTYNAYANG@users.noreply.github.com>
2023-08-14 12:56:10 +0800

    Add files via upload

---
+++ Operations.py
@@ -0,0 +1,33 @@
+#OPERATIONS
+#returns the remaining value after division
+print(10%3)
+
+#returns the answer without decimals
+print(10//3)
+
+#Warm UP Questions
+print("question 5 = (20/4)+8 = " + str(int(20/4)) + " + 8 = " + str(int((20/4)+8)))
+print("question 7 = (7*3)-(2*4) = " + str(7*3) + " - " + str(2*4) + " = " + str((7*3)-(2*4)))
+print(f"question 2 = (5+4)*3) = {str(5+4)} - 3 = {str((5+4)*3)}")
+print(f"question 4 = (9-6)*8) = {str(9-6)} - 3 = {str((9-6)*8)}")
+
+#christmas day
+cd = 336
+#normal day
+nd = 280
+#passenger
+p = 240
+#PLANE Questions
+print("Question 1 = " + str(cd/12))
+print("Question 2 = " + str(p*(cd/12)))
+print("Question 3 = " + str((p*cd)-(p*nd)))
+print("Question 4 = " + str(1835-782))
+print("Question 5 = " + str(832/32*5))
+print("Question 6 = 7,580 + x = 12997, x = " + str(12997-7580))
+
+#Square Root
+import math
+x = math.sqrt(64)
+print(x)
+
+print(math.sqrt(17**2-8**2))
