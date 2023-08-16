9f22917819dddf443d68e249b73d16c1ac684aa2
JUSTYNAYANG <103252328+JUSTYNAYANG@users.noreply.github.com>
2023-08-16 09:54:24 +0800

    Add files via upload

---
+++ siriusly.py
@@ -0,0 +1,17 @@
+print("Hello peasants.  As GENEROUS as I am, I, Siriusly, will spare a few seconds of my time to answer \n"
+      "your unsophisticated questions.  Your welcome, luckily for you, I am just that GREAT.\n")
+
+q1 = input("Well?  What are you waiting for, I don't have all day ")
+print(f"'{q1}' Seriously? That's your question? Are you purposely trying to waste my time?\n")
+
+input("You're lucky, I'm in a good mood, ask another question. ")
+
+input("EWWW! Why should I answer that, it so BASIC. Next! ")
+
+print("Stop asking these uncultured questions? What is wrong with you?\n")
+input("Such a disappointment.  Continue, THINK HARD before you ask ")
+
+print("THAT'S IT! Don't drag me into you STUPIDITY!\n")
+input("*takes a deep breath* Last chance, ASK ")
+
+print("Failure.  Be grateful I graced you with my presence today. Such a WASTE of my PRECIOUS time.\n")
